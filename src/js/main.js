5ed34c70195776a4c2fa39974dcae2c22575cd1d
Dariya Maslyukova <darija0207@ukr.net>
2022-07-25 13:37:05 +0300

    Slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+$(document).ready(function() {
+
+  const owl = $('.services-slider .owl-carousel');
+  const info = $('.services-slider-number');
+
+  owl.owlCarousel({
+    animateOut: 'fadeOut',
+    items: 1,
+    smartSpeed: 450,
+    nav: true,
+    loop: true,
+    onInitialized: function(event) {
+      nextSlide(event);
+    }
+  });
+
+  owl.on('changed.owl.carousel', function(event) {
+    const index = event.page.index + 1;
+    info.text(index < 10 ? `0${index}` : index);
+
+    nextSlide(event);
+  });
+
+  $('.services-slider-next').click(function() {
+    owl.trigger('next.owl.carousel');
+  })
 
 });
 
+function nextSlide(event) {
+  const current = event.item.index;
+  const nextSlide = $(event.target).find(".owl-item").eq(current + 1).html();
 
+  const nextSlideSrc = $(nextSlide).find('img').attr('src');
+  const nextSlideTitle = $(nextSlide).find('.services-slider__info__title').text();
+
+  $('.services-slider-next__img').attr('style', `background-image: url(${nextSlideSrc})`)
+  $('.services-slider-next__info__title').text(nextSlideTitle);
+}
